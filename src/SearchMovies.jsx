2ab147e92ca251fe013a48cdc21f069af3895088
“Saibal <connectsaibalroy@gmail.com>
2020-08-01 14:09:42 +0530

    initial commit

---
+++ src/SearchMovies.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchMovies = () => {
+    return ( 
+        <form className="form">
+            <label className="label" htmlFor="query">Movie Name</label>
+            <input className="input" type="text" name="query" placeholder="i.e. Jurassic Park"/>
+            <button className="button" type="submit">Search</button>
+        </form>
+     );
+}
+ 
+export default SearchMovies;
